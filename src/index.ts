cdc79fb2f97651d28fb5fab18db2fc0e54b561cc
TarinPairor <111269625+TarinPairor@users.noreply.github.com>
2025-02-01 22:27:56 +0800

    Toggle to see change

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.get("/", (_: Request, res: Response) => {
   res.send("Express + TypeScript Server");
