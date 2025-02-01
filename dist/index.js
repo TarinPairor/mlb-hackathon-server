556d8c3bd879d73cc9484db2e1195483dcf04ed4
TarinPairor <111269625+TarinPairor@users.noreply.github.com>
2025-02-01 22:45:32 +0800

    lol npm run build take the l

--- dist/index.js
+++ dist/index.js
@@ -6,12 +6,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // src/index.ts
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const cors_1 = __importDefault(require("cors"));
+const api_1 = __importDefault(require("./routes/api"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3002;
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.use("/api", api_1.default);
 app.get("/", (_, res) => {
     res.send("Express + TypeScript Server");
 });
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
 });
+exports.default = app;
