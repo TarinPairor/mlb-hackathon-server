ea0483830021e1e77d70b36f97a76086abf0411a
TarinPairor <111269625+TarinPairor@users.noreply.github.com>
2025-02-02 22:28:42 +0800

    Add 2024MLBHomeRuns

--- dist/routes/api.js
+++ dist/routes/api.js
@@ -5,9 +5,26 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const dotenv_1 = __importDefault(require("dotenv"));
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
+const csv_parser_1 = __importDefault(require("csv-parser"));
 dotenv_1.default.config();
 const router = (0, express_1.Router)();
 router.get("/", (req, res) => {
     res.json({ message: "Api Route Working" });
+});
+router.get("/getSampleOf2024MLBHomeRuns", (req, res) => {
+    const results = [];
+    const filePath = path_1.default.join(__dirname, "../data/2024-mlb-homeruns.csv");
+    fs_1.default.createReadStream(filePath)
+        .pipe((0, csv_parser_1.default)())
+        .on("data", (data) => {
+        if (results.length < 10) {
+            results.push(data.video);
+        }
+    })
+        .on("end", () => {
+        res.json(results);
+    });
 });
 exports.default = router;
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,5 +1,8 @@
 import { Router, Request, Response } from "express";
 import dotenv from "dotenv";
+import fs from "fs";
+import path from "path";
+import csv from "csv-parser";
 
 dotenv.config();
 
@@ -7,6 +10,22 @@ const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
   res.json({ message: "Api Route Working" });
+});
+
+router.get("/getSampleOf2024MLBHomeRuns", (req: Request, res: Response) => {
+  const results: string[] = [];
+  const filePath = path.join(__dirname, "../data/2024-mlb-homeruns.csv");
+
+  fs.createReadStream(filePath)
+    .pipe(csv())
+    .on("data", (data) => {
+      if (results.length < 10) {
+        results.push(data.video);
+      }
+    })
+    .on("end", () => {
+      res.json(results);
+    });
 });
 
 export default router;
